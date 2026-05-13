3cc95d5ef9b5eeadd2a98ec30ccdbcf44dceafca
Suraj Mandal <rjsurajmandal@gmail.com>
2026-05-13 13:30:28 +0530

    updated

--- libraryQuestions.sql
+++ libraryQuestions.sql
@@ -10,3 +10,27 @@ CREATE table book2 as (
 );
  
 SELECT accession_no,book_title FROM book2;
+
+-- give book with no titles
+
+SELECT *
+FROM combinedlibrary
+WHERE book_title IS NULL
+   OR TRIM(book_title) = '';
+
+
+
+-- give book with no publisher
+
+SELECT *
+FROM books
+WHERE publisher IS NULL
+   OR TRIM(publisher) = '';
+
+
+-- give book with  no book shelf no.
+
+SELECT *
+FROM books
+WHERE book_shelf_no IS NULL
+   OR TRIM(book_shelf_no) = '';
